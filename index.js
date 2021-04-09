86030a7bfc50c3d6bb03c3a51992ef42fc00a2ed
Kenny Boyer <ksboyerii@gmail.com>
2021-04-09 12:03:53 -0500

    added html generation and js

--- index.js
+++ index.js
@@ -43,16 +43,12 @@ function init() {
 				message:'Where is your office?',
 			}])
 	
-		.then((answers) => {
-			console.log(answers);
-	
-		})
+		.then( ( { name, id, email, office } ) => {
+			const manager = new Manager( name, id, email, office );
+			teamMembers.push( manager );
+			createTeam();
+		} )
 	}
-		// prompt user with questions needed to satisfy the input for a manager object
-		// .then statement
-			// create a new instance of the Manager class
-			// push the new manager object to the team members array
-			// call the function to create the rest of the team
 
 
   
@@ -62,12 +58,99 @@ function init() {
 		// .then statement
 			// if a type of employee was selected, call function to add that type of employee
 			// if the other option was selected, call the function to create the output
+	function createTeam() {
+		inquirer.prompt(
+			[{
+				type: 'list',
+				name: 'role',
+				message:'What type of team member would you like to add?',
+				choices: [
+					"Manager","Engineer","Intern","None"
+				]
+			}
+		]
+		).then( ( { role } ) => {
+			// if else statements
+			// if a type of employee was selected, call function to add that type of employee
+			// if the other option was selected, call the function to create the output
+			//
+			 if (role === "Engineer") {
+			createEngineer();
+			} else if (role === "Intern") {
+				createIntern();
+			} else if(role === "Manager"){ 
+				createManager();
+			} else{
+				console.log("Have a nice day!");
+			}
+		})
+
+	}
+	
 
 	// function to add an engineer
 		// same idea as create manager
+	function createEngineer() {
+		inquirer.prompt(
+			[{
+				type: 'input',
+				name: 'name',
+				message:'What is your name?',
+			},
+			{
+				type: 'input',
+				name: 'email',
+				message:'What is your email?',
+			},
+			{
+				type: 'input',
+				name: 'id',
+				message:'What is your ID?',
+	
+			},
+			{
+				type: 'input',
+				name: 'github',
+				message:'What is your GitHub username?',
+			}]
+		).then( ( { name, id, email, github } ) => {
+			const engineer = new Engineer( name, id, email, github );
+			teamMembers.push( engineer );
+			createTeam();
+		})
+	}
   
 	// function to add an intern
 		// same idea as create manager
+		function createIntern() {
+			inquirer.prompt(
+				[{
+					type: 'input',
+					name: 'name',
+					message:'What is your name?',
+				},
+				{
+					type: 'input',
+					name: 'email',
+					message:'What is your email?',
+				},
+				{
+					type: 'input',
+					name: 'id',
+					message:'What is your ID?',
+		
+				},
+				{
+					type: 'input',
+					name: 'school',
+					message:'What school do you attend?',
+				}]
+			).then( ( { name, id, email, school } ) => {
+				const engineer = new Engineer( name, id, email, school );
+				teamMembers.push( engineer );
+				createTeam();
+			})
+		}
 
 	// function to create the output
 		// call the function from page-template module and pass in the team members array and save to a data variable
